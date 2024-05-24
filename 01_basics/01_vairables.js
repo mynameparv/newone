c67a22d993a4bc7bb916886160d499fda5b3aebb
mynameparv <parvajain327p@gmail.com>
2024-05-24 13:12:33 +0530

    commit config creation

---
+++ 01_basics/01_vairables.js
@@ -0,0 +1,8 @@
+const accountId=12999;
+let mailId="parvajain@gmail.com";
+
+var accountPassword="parv@1501";
+
+mailId="parva@gamil.com";
+accountPassword="par@1501";
+console.table([accountId,mailId,accountPassword]);
